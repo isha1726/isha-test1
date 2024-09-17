76d33b08d2beab4c2551d674f3279597fa31b5c2
isha <ishpandey1917@gmail.com>
2024-09-17 15:21:39 +0530

    ishap001 - Added isOddOrEven function in Utility.js

---
+++ src/main/js/com/example/javarmavenjunithelloworld/Utility.js
@@ -0,0 +1,10 @@
+function isOddOrEven(number) {
+    if (number % 2 === 0) {
+        console.log(number + " is even");
+    } else {
+        console.log(number + " is odd");
+    }
+}
+
+// Example test call
+isOddOrEven(5);  // Output: 5 is odd
